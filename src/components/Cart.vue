1249ab84d9a2f743c0950b8aab8858902e31d15c
Imtiaz Shawn <Imtiazshawn01@gmail.com>
2023-09-13 17:29:08 +0600

    Subtotal

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -37,7 +37,7 @@
                     <li>TOTAL ITEMS</li>
                     <li>{{ cartItemCount }}</li>
                     <li>TOTAL</li>
-                    <li>100.00</li>
+                    <li>{{ subtotal }}</li>
                 </ul>
             </div>
             <div class="cartDetails__footer-discount">
--- src/stores/cart.js
+++ src/stores/cart.js
@@ -1,31 +1,48 @@
-import { defineStore } from 'pinia'
+import { defineStore } from 'pinia';
 
-const CART_KEY = 'cartData'
+const CART_KEY = 'cartData';
 
 export const useCartStore = defineStore('cart', {
   state: () => ({
-    cart: []
+    cart: [],
+    subtotal: 0,
   }),
   actions: {
     addToCart(product) {
-      this.cart.push(product)
-      this.saveCartData()
+      this.cart.push(product);
+      this.saveCartData();
+      this.updateSubtotal();
     },
     removeFromCart(product) {
-      const index = this.cart.findIndex((item) => item.id === product.id)
+      const index = this.cart.findIndex((item) => item.id === product.id);
       if (index !== -1) {
-        this.cart.splice(index, 1)
-        this.saveCartData()
+        this.cart.splice(index, 1);
+        this.saveCartData();
+        this.updateSubtotal();
       }
     },
     saveCartData() {
-      localStorage.setItem(CART_KEY, JSON.stringify(this.cart))
+      localStorage.setItem(CART_KEY, JSON.stringify(this.cart));
     },
     loadCartData() {
-      const cartData = localStorage.getItem(CART_KEY)
+      const cartData = localStorage.getItem(CART_KEY);
       if (cartData) {
-        this.cart = JSON.parse(cartData)
+        this.cart = JSON.parse(cartData);
+        this.cart.forEach((product) => {
+          if (!product.quantity) {
+            product.quantity = 1;
+          }
+        });
+        this.updateSubtotal();
       }
-    }
-  }
-})
+    },
+    updateSubtotal() {
+      let total = 0;
+      for (const product of this.cart) {
+        total += product.price * product.quantity;
+      }
+      this.subtotal = total.toFixed(2);
+      console.log(this.subtotal);
+    },
+  },
+});
